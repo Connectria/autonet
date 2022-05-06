e3e710ff9e4de1fa289100f0cb370f19013cee5a
Ken Vondersaar <54597358+kvondersaar@users.noreply.github.com>
2022-05-06 17:15:24 -0500

    fix missing arg in get_interfaces()

--- autonet/blueprints/interfaces.py
+++ autonet/blueprints/interfaces.py
@@ -1,17 +1,37 @@
 from flask import Blueprint, g
 
+from autonet.core import exceptions as exc
+from autonet.core import objects as obj
 from autonet.core.response import autonet_response
+
 
 blueprint = Blueprint('interfaces', __name__)
 
 
 @blueprint.route('', methods=['GET'])
 def get_interfaces(device_id):
+    def verify(driver_response):
+        if not isinstance(driver_response, list):
+            return False
+        for item in driver_response:
+            if not isinstance(item, obj.interfaces.Interface):
+                return False
+        return True
+
     response = g.driver.execute('interface', 'read')
-    return autonet_response(response, 200)
+    if verify(response):
+        return autonet_response(response)
+    else:
+        raise exc.DriverResponseInvalid(g.driver)
 
 
 @blueprint.route('/<interface_name>', methods=['GET'])
-def get_interface(interface_name):
+def get_interface(device_id, interface_name):
+    def verify(driver_response):
+        return isinstance(driver_response, obj.interfaces.Interface)
+
     response = g.driver.execute('interface', 'read', request_data=interface_name)
-    return autonet_response(response, 200)
+    if verify(response):
+        return autonet_response(response)
+    else:
+        raise exc.DriverResponseInvalid(g.driver)
