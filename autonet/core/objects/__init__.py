260cce892cc6c3c3da2acc40578d819e973ccecc
Ken Vondersaar <54597358+kvondersaar@users.noreply.github.com>
2022-05-06 13:58:14 -0500

    import submodules in __init__.py

--- autonet/core/objects/__init__.py
+++ autonet/core/objects/__init__.py
@@ -3,3 +3,4 @@ Here we use python dataclasses to define the structure of
 given endpoints request/response data as well as implement
 validation logic in the `__post_init__()` method.
 """
+import autonet.core.objects.interfaces as interfaces
