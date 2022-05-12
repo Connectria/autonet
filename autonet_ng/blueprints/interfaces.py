b3a2994c30148a7a5ae39464ae7969cd3707a3c5
Ken Vondersaar <54597358+kvondersaar@users.noreply.github.com>
2022-05-12 09:29:59 -0500

    Created interface_create endpoint.

--- autonet_ng/blueprints/interfaces.py
+++ autonet_ng/blueprints/interfaces.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, g
+from flask import Blueprint, g, request
 
 from autonet_ng.core import exceptions as exc
-from autonet_ng.core import objects as obj
+from autonet_ng.core.objects import interfaces as an_if
 from autonet_ng.core.response import autonet_response
 
 
@@ -14,7 +14,7 @@ def get_interfaces(device_id):
         if not isinstance(driver_response, list):
             return False
         for item in driver_response:
-            if not isinstance(item, obj.interfaces.Interface):
+            if not isinstance(item, an_if.Interface):
                 return False
         return True
 
@@ -27,20 +27,30 @@ def get_interfaces(device_id):
 
 @blueprint.route('/<interface_name>', methods=['GET'])
 def get_interface(device_id, interface_name):
-    def verify(driver_response):
-        return isinstance(driver_response, obj.interfaces.Interface)
-
     response = g.driver.execute('interface', 'read', request_data=interface_name)
-    if verify(response):
+    if isinstance(response, an_if.Interface):
         return autonet_response(response)
     else:
         raise exc.DriverResponseInvalid(g.driver)
 
-#
-# @blueprint.route('/<interface_name>', methods=['GET'])
-# def get_interface(device_id, interface_name):
-#     def verify(driver_response):
-#         return isinstance(driver_response, obj.interfaces.Interface)
-#
-#     def generate_objects():
-#         pass
+
+@blueprint.route('/', methods=['POST'])
+def create_interface(device_id):
+    # Depending on the interface mode, the attributes value
+    # may be one of 2 classes, or None.
+    request_data = request.json
+    if request_data['mode'] == 'routed':
+        request_data['attributes'] = an_if.InterfaceRouteAttributes(**request.json['attributes'])
+    elif request_data['mode'] == 'bridged':
+        request_data['attributes'] = an_if.InterfaceBridgeAttributes(**request.json['attributes'])
+    else:
+        # Maybe some got sent, but we'll silently ignore them.
+        request_data['attributes'] = None
+
+    request_data = an_if.Interface(**request_data)
+
+    response = g.driver.execute('interface', 'create', request_data=request_data)
+    if isinstance(response, an_if.Interface):
+        return autonet_response(response)
+    else:
+        raise exc.DriverResponseInvalid(g.driver)
