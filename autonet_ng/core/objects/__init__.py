222026d0013924fd6c33286d93bbcbe0da07fdf6
Ken Vondersaar <54597358+kvondersaar@users.noreply.github.com>
2022-05-17 00:27:41 -0500

    Reworked module to avoid circular imports.

--- autonet_ng/core/objects/__init__.py
+++ autonet_ng/core/objects/__init__.py
@@ -3,4 +3,3 @@ Here we use python dataclasses to define the structure of
 given endpoints request/response data as well as implement
 validation logic in the `__post_init__()` method.
 """
-import autonet_ng.core.objects.interfaces as interfaces
