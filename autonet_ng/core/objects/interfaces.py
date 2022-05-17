4c87d2250be9f5fd666c3ea2227203e2cf2e19cc
Ken Vondersaar <54597358+kvondersaar@users.noreply.github.com>
2022-05-17 00:25:19 -0500

    Better validation on interface objects.

--- autonet_ng/core/objects/interfaces.py
+++ autonet_ng/core/objects/interfaces.py
@@ -3,7 +3,7 @@ import macaddress
 
 from dataclasses import dataclass, field
 from ipaddress import ip_interface
-from typing import Union
+from typing import Optional, Union
 
 import autonet_ng.core.exceptions as exc
 import autonet_ng.core.objects.validators as v
@@ -33,44 +33,59 @@ class InterfaceAddress(object):
             raise exc.RequestValueError('address', self.address)
         # Verify virtual type, as appropriate.
         if self.virtual and self.virtual_type not in valid_virtual_types:
-            raise exc.RequestValueError('family', self.virtual_type,
+            raise exc.RequestValueError('virtual_type', self.virtual_type,
                                         valid_values=valid_virtual_types)
 
 
 @dataclass
 class InterfaceBridgeAttributes(object):
     dot1q_enabled: bool
-    dot1q_vids: list[int]
-    dot1q_pvid: int
+    dot1q_vids: list[int] = field(default_factory=list)
+    dot1q_pvid: Optional[int] = field(default=None)
 
     def __post_init__(self):
         v.validate(self)
+
+    def merge(self, update: 'InterfaceBridgeAttributes'):
+        if update.dot1q_enabled is not None:
+            self.dot1q_enabled = update.dot1q_enabled
+        if update.dot1q_pvid is not None:
+            self.dot1q_pvid = update.dot1q_pvid
+        if update.dot1q_vids is not None and update.dot1q_enabled:
+            self.dot1q_vids = update.dot1q_vids
 
 
 @dataclass
 class InterfaceRouteAttributes(object):
-    vrf: Union[str, None]
     addresses: list[InterfaceAddress]
+    vrf: Optional[str] = field(default=None)
 
     def __post_init__(self):
         v.validate(self)
 
+    def merge(self, update: 'InterfaceRouteAttributes'):
+        if update.vrf is not None:
+            self.vrf = update.vrf
+        if update.addresses is not None:
+            # For addresses, we convert to a set for the merge action so that
+            # duplicates are removed. Then cast back to a list on assignment.
+            self.addresses = list(set(self.addresses).union(set(update.addresses)))
+
 
 @dataclass
 class Interface(object):
-    name: str
-    mode: str
-    description: str
-    virtual: bool
-    mode: str
-    attributes: Union[InterfaceBridgeAttributes, InterfaceRouteAttributes, None]
-    admin_enabled: bool = field(default=True)
-    physical_address: str = field(default='00:00:00:00:00:00')
+    name: Optional[str] = field(default=None)
+    mode: Optional[str] = field(default=None)
+    description: Optional[str] = field(default=None)
+    virtual: Optional[bool] = field(default=None)
+    attributes: Optional[Union[InterfaceBridgeAttributes, InterfaceRouteAttributes]] = field(default=None)
+    admin_enabled: Optional[bool] = field(default=None)
+    physical_address: Optional[str] = field(default=None)
     child: bool = field(default=False)
-    parent: Union[str, None] = field(default=None)
-    speed: Union[int, None] = field(default=None)
-    duplex: Union[str, None] = field(default=None)
-    mtu: int = field(default=1500)
+    parent: Optional[str] = field(default=None)
+    speed: Optional[int] = field(default=None)
+    duplex: Optional[str] = field(default=None)
+    mtu: Optional[int] = field(default=None)
 
     def __post_init__(self):
         v.validate(self)
@@ -78,15 +93,16 @@ class Interface(object):
         valid_modes = ['routed', 'bridged', 'aggregated']
         # Validate physical address and enforce format
         try:
-            self.physical_address = str(macaddress.parse(
-                self.physical_address, macaddress.EUI48))
+            if self.physical_address:
+                self.physical_address = str(macaddress.parse(
+                    self.physical_address, macaddress.EUI48))
         except Exception:
             raise exc.RequestValueError('physical_address', self.physical_address)
         # Validate parent is set if child is true.
         if self.child and not self.parent:
             raise exc.RequestValueError('parent', self.parent)
         # Validate modes
-        if self.mode not in valid_modes:
+        if self.mode and self.mode not in valid_modes:
             raise exc.RequestValueError('parent', self.mode, valid_values=valid_modes)
         # Validate attribute matches mode
         if self.mode == 'routed' and not isinstance(
@@ -98,3 +114,24 @@ class Interface(object):
         if self.mode == 'aggregated' and self.attributes:
             raise exc.RequestTypeError('mode', self.attributes, type(self.attributes),
                                        None)
+
+    def merge(self, update: 'Interface'):
+        """
+        Performs a merge of the "updated" object into this one.  Any attributes
+        on `update` that are set to `None` will be ignored.
+        :param update:
+        :return:
+        """
+        special_consideration = ['mode', 'attributes']
+        for key in update.__annotations__:
+            new_value = getattr(update, key, None)
+            if new_value is not None and key not in special_consideration:
+                setattr(self, key, new_value)
+        # If the mode is the same, or likewise, the attribute object passed is
+        # of the same time then perform a merge.  Otherwise, a mode change is being
+        # performed, and so overwrite these attributes.
+        if self.mode == update.mode or isinstance(update.attributes, type(self.attributes)):
+            self.attributes.merge(update.attributes)
+        else:
+            self.mode = update.mode
+            self.attributes = update.attributes
