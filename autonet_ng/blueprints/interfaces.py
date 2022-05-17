995b2ae7bf4f54c13e0513c4cf1d74877bf30853
Ken Vondersaar <54597358+kvondersaar@users.noreply.github.com>
2022-05-17 00:26:12 -0500

    Implemented interface create, and pre-validation.

--- autonet_ng/blueprints/interfaces.py
+++ autonet_ng/blueprints/interfaces.py
@@ -36,13 +36,28 @@ def get_interface(device_id, interface_name):
 
 @blueprint.route('/', methods=['POST'])
 def create_interface(device_id):
+    # Verify minimum data is sent with request
+    for field in ['name', 'attributes', 'mode']:
+        if request.json.get(field, None) is None:
+            raise exc.RequestValueMissing(field)
+
+    # Now we set default values as appropriate.
+    request_data = request.json
+    request_data['admin_enabled'] = request_data.get('admin_enabled', True)
+    request_data['mtu'] = request_data.get('mtu', 1500)
+    # NOTE: speed, duplex, virtual, parent and child fields are
+    # up to the driver to figure out based on the name.  They may be
+    # provided in the request and silently ignored.
+
     # Depending on the interface mode, the attributes value
     # may be one of 2 classes, or None.
-    request_data = request.json
     if request_data['mode'] == 'routed':
-        request_data['attributes'] = an_if.InterfaceRouteAttributes(**request.json['attributes'])
+        request_data['attributes']['addresses'] = [an_if.InterfaceAddress(**address)
+                                                   for address
+                                                   in request_data['attributes']['addresses']]
+        request_data['attributes'] = an_if.InterfaceRouteAttributes(**request_data['attributes'])
     elif request_data['mode'] == 'bridged':
-        request_data['attributes'] = an_if.InterfaceBridgeAttributes(**request.json['attributes'])
+        request_data['attributes'] = an_if.InterfaceBridgeAttributes(**request_data['attributes'])
     else:
         # Maybe some got sent, but we'll silently ignore them.
         request_data['attributes'] = None
