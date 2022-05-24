154c4bc46c05ef780af668f54202efe893fe5917
Ken Vondersaar <54597358+kvondersaar@users.noreply.github.com>
2022-05-24 08:41:55 -0500

    add in driver response validation.

--- autonet_ng/blueprints/vrf.py
+++ autonet_ng/blueprints/vrf.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, g, request
 
+from autonet_ng.core import exceptions as exc
 from autonet_ng.core.objects import vrf as an_vrf
 from autonet_ng.core.response import autonet_response
 
@@ -8,25 +9,40 @@ blueprint = Blueprint('vrf', __name__)
 
 @blueprint.route('/', methods=['GET'])
 def get_vrfs(device_id):
-    result = g.driver.execute('vrf', 'read')
-    return autonet_response(result)
+    def verify(driver_response):
+        if not isinstance(driver_response, list):
+            return False
+        for item in driver_response:
+            if not isinstance(item, an_vrf.VRF):
+                return False
+        return True
+
+    response = g.driver.execute('vrf', 'read')
+    if not verify(response):
+        raise exc.DriverResponseInvalid(g.driver)
+    return autonet_response(response)
 
 
 @blueprint.route('/<vrf_name>', methods=['GET'])
 def get_vrf(device_id, vrf_name):
-    result = g.driver.execute('vrf', 'read', request_data=vrf_name)
-    return autonet_response(result)
+    response = g.driver.execute('vrf', 'read', request_data=vrf_name)
+    if not isinstance(response, an_vrf.VRF):
+        raise exc.DriverResponseInvalid(g.driver)
+    return autonet_response(response)
 
 
 @blueprint.route('/', methods=['POST'])
 def create_vrf(device_id):
     vxlan = an_vrf.VRF(**request.json)
-
-    result = g.driver.execute('vrf', 'create', request_data=vxlan)
-    return autonet_response(result)
+    response = g.driver.execute('vrf', 'create', request_data=vxlan)
+    if not isinstance(response, an_vrf.VRF):
+        raise exc.DriverResponseInvalid(g.driver)
+    return autonet_response(response)
 
 
 @blueprint.route('/<vrf_name>', methods=['DELETE'])
 def delete_vrf(device_id, vrf_name):
-    g.driver.execute('vrf', 'delete', request_data=vrf_name)
+    response = g.driver.execute('vrf', 'delete', request_data=vrf_name)
+    if response is not None:
+        raise exc.DriverResponseInvalid(g.driver)
     return autonet_response(None, 204)
