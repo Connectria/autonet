adf79dbfee9b4dcfd0a9df7941be52793a0dbf00
Ken Vondersaar <54597358+kvondersaar@users.noreply.github.com>
2022-05-26 14:35:01 -0500

    Add LAG object.

---
+++ autonet_ng/core/objects/lag.py
@@ -0,0 +1,25 @@
+import re
+
+from dataclasses import dataclass, field
+from typing import List, Optional
+
+from autonet_ng.core import exceptions as exc
+from autonet_ng.core.objects import validators as v
+
+
+@dataclass
+class LAG(object):
+    name: str = field(default=None)
+    members: Optional[List[str]] = field(default_factory=List)
+    evpn_esi: Optional[str] = field(default=None)
+
+    def __post_init__(self):
+        # 'auto' is a valid esi option for platforms that support type3 ESI.
+        # Drivers that do not support Type3 ESI need to report upstream as such.
+        if self.evpn_esi and self.evpn_esi != 'auto':
+            if not v.is_esi(self.evpn_esi):
+                raise exc.RequestValueError('evpn_esi', self.evpn_esi)
+            # replace common separators, cast to bytes then cast
+            # back to a well formatted string.
+            self.evpn_esi = bytes.fromhex(re.sub(r'[-_.:]', '', self.evpn_esi)).hex(':')
+        v.validate(self)
